40a4ae94a8ef2d496a95735e014c7beec2f68d7f
Vikram Kriplaney <markiv@gmail.com>
2022-09-13 13:24:41 +0200

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Vikram Kriplaney on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

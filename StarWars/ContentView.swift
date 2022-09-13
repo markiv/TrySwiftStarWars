7dc0656d74cf1079ae20e49af276b9c8d407c691
Vikram Kriplaney <markiv@gmail.com>
2022-09-13 13:43:43 +0200

    Fetch a single "People" with await

--- StarWars/ContentView.swift
+++ StarWars/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var people: People?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if let people {
+            List {
+                Text(people.name)
+            }
+        } else {
+            ProgressView("Loading...")
+                .task {
+                    do {
+                        let (data, _) = try await URLSession.shared
+                            .data(from: URL(string: "https://swapi.dev/api/people/1/")!)
+                        people = try JSONDecoder().decode(People.self, from: data)
+                    } catch {
+                        print(error)
+                    }
+                }
         }
-        .padding()
     }
 }
 
